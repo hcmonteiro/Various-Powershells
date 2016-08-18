cd72cff14edcacf830070a1cebc7ccb18d5bb378
Hugo Monteiro <hmmonteiro@jp-ik.com>
2016-08-18 12:38:24 +0100

    Add Powershell Remove Printer
    Removes a printer of a specific Print Server

---
+++ README.md
@@ -0,0 +1,1 @@
+Various Powershells Used by me
---
+++ RemovePrinter.ps1
@@ -0,0 +1,3 @@
+﻿$a = Get-WMIObject -query "Select * From Win32_Printer Where ShareName = 'HQ-MFP-CO-A3-21'"
+$a.SetDefaultPrinter()
+Remove-Printer *srv-print*
