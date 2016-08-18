ce2e10dd6d9e02198ea03cb040b10fd877e561f4
Hugo Monteiro <hmmonteiro@jp-ik.com>
2016-08-18 12:54:40 +0100

    Change default printer and deletes all printers from Print Server
    Change default printer and deletes all printers from Print Server

--- RemovePrinter.ps1
+++ RemovePrinter.ps1
@@ -1,3 +1,24 @@
-﻿$a = Get-WMIObject -query "Select * From Win32_Printer Where ShareName = 'HQ-MFP-CO-A3-21'"
+﻿########################################################
+# Name: RemovePrinter.ps1                              
+# Creator: Hugo Monteiro
+# CreationDate: 18.08.2016                              
+# LastModified: 18.08.2016
+# Version: 1.0                                         
+# Doc: https://github.com/hcmonteiro/Various-Powershells/
+#
+# First it sets the default printer to HQ-MFP-CO-A3-21 (change to your printer)
+# Then it removes all the printers from the print server srv-print
+# 
+# 
+# 
+#   
+#
+# Version 1.0
+########################################################
+#
+# hcmonteiro@live.com
+#
+########################################################
+$a = Get-WMIObject -query "Select * From Win32_Printer Where ShareName = 'HQ-MFP-CO-A3-21'"
 $a.SetDefaultPrinter()
 Remove-Printer *srv-print*
