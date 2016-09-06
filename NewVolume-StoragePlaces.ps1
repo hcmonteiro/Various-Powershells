be49feb1e31616054039f4fab1678b4af9f3c7a3
Hugo Monteiro <hmmonteiro@jp-ik.com>
2016-09-06 09:08:36 +0100

    Create a Pool and Disk on StoragePlaces
    Create a new Pool on StoragePlaces.
    Also a new Disk.

---
+++ NewVolume-StoragePlaces.ps1
@@ -0,0 +1,30 @@
+﻿#
+# Criação de novo disco na Pool
+#
+$SSD = Get-StorageTier -FriendlyName Microsoft_SSD_Template
+$HDD = Get-StorageTier -FriendlyName Microsoft_HDD_Template
+$DISKNAME = "DiskWNONOC1GB"
+New-VirtualDisk -StoragePoolFriendlyName "Pool01" -FriendlyName $DISKNAME -StorageTiers @($SSD, $HDD) -StorageTierSizes @(100GB, 1TB) -ResiliencySettingName Mirror -WriteCacheSize 1GB
+
+Start-Sleep -Second 5
+
+Get-VirtualDisk $DISKNAME  | Get-Disk | Set-Disk -IsReadOnly 0
+
+Get-VirtualDisk $DISKNAME  | Get-Disk | Set-Disk -IsOffline 0
+
+Get-VirtualDisk $DISKNAME  | Get-Disk | Initialize-Disk -PartitionStyle GPT
+
+Start-Sleep -Second 2
+
+Get-VirtualDisk $DISKNAME  | Get-Disk | New-Partition -DriveLetter “L” -UseMaximumSize
+
+Start-Sleep -Second 2
+
+Initialize-Volume -DriveLetter “L” -FileSystem NTFS -Confirm:$false
+
+Start-Sleep -Second 2
+
+Set-Volume -DriveLetter L -NewFileSystemLabel $DISKNAME
+
+#Get-StorageTier -FriendlyName *HDD*
+
